9abace0b4010937a366325897e39885af65d046c
roman-pixel <romansrgvch@outlook.com>
2023-10-27 22:10:15 +0300

    optimizing

--- js/script.js
+++ js/script.js
@@ -8,27 +8,70 @@ yearEl.textContent = currentYear;
 // Mobile nav
 const btnNavEl = document.querySelector(".btn-mobile-nav");
 const headerEl = document.querySelector(".header");
+const htmlEl = document.querySelector("html");
 
 btnNavEl.addEventListener("click", function () {
   headerEl.classList.toggle("nav-open");
+
+  // Disable scrolling when navigation is open
+  if (headerEl.classList.contains("nav-open")) {
+    htmlEl.style.overflow = "hidden";
+  } else {
+    htmlEl.style.overflow = "auto";
+  }
 });
 
 ///////////////////////////////////////////////////////////
 // Smooth scrolling animation
-// const allLinks = document.querySelectorAll("a:link");
-// allLinks.forEach(function (link) {
-//   link.addEventListener("click", function (e) {
-//     e.preventDefault;
-//     const href = link.getAttribute("href");
+const allLinks = document.querySelectorAll("a:link");
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    // e.preventDefault;
+    // const href = link.getAttribute("href");
 
-//     // Scroll back to top
-//     if (href === "#")
-//       window.scrollTo({
-//         top: 0,
-//         behavior: "smooth",
-//       });
-//   });
-// });
+    // // Scroll back to top
+    // if (href === "#")
+    //   window.scrollTo({
+    //     top: 0,
+    //     behavior: "smooth",
+    //   });
+
+    //   // Scroll to other links
+    // if (href !== "#" && href.startsWith("#")) {
+    //   const sectionEl = document.querySelector(href);
+    //   sectionEl.scrollIntoView({ behavior: "smooth" });
+    // }
+
+    // Close mobile nav
+    if (link.classList.contains("main-nav-link")) {
+      headerEl.classList.toggle("nav-open");
+      htmlEl.style.overflow = "auto";
+    }
+  });
+});
+
+///////////////////////////////////////////////////////////
+// Sticky navigation
+const sectionHeroEl = document.querySelector(".section-hero");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+
+    if (!ent.isIntersecting) {
+      document.body.classList.add("sticky");
+    } else {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    // In the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionHeroEl);
 
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
@@ -51,54 +94,3 @@ function checkFlexGap() {
 checkFlexGap();
 
 // https://unpkg.com/smoothscroll-polyfill@0.4.4/dist/smoothscroll.min.js
-
-/*
-.no-flexbox-gap .main-nav-list li:not(:last-child) {
-  margin-right: 4.8rem;
-}
-
-.no-flexbox-gap .list-item:not(:last-child) {
-  margin-bottom: 1.6rem;
-}
-
-.no-flexbox-gap .list-icon:not(:last-child) {
-  margin-right: 1.6rem;
-}
-
-.no-flexbox-gap .delivered-faces {
-  margin-right: 1.6rem;
-}
-
-.no-flexbox-gap .meal-attribute:not(:last-child) {
-  margin-bottom: 2rem;
-}
-
-.no-flexbox-gap .meal-icon {
-  margin-right: 1.6rem;
-}
-
-.no-flexbox-gap .footer-row div:not(:last-child) {
-  margin-right: 6.4rem;
-}
-
-.no-flexbox-gap .social-links li:not(:last-child) {
-  margin-right: 2.4rem;
-}
-
-.no-flexbox-gap .footer-nav li:not(:last-child) {
-  margin-bottom: 2.4rem;
-}
-
-@media (max-width: 75em) {
-  .no-flexbox-gap .main-nav-list li:not(:last-child) {
-    margin-right: 3.2rem;
-  }
-}
-
-@media (max-width: 59em) {
-  .no-flexbox-gap .main-nav-list li:not(:last-child) {
-    margin-right: 0;
-    margin-bottom: 4.8rem;
-  }
-}
-*/
